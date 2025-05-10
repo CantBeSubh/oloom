8d75004ecd33de717a5744dc16925bef046e58fa
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-05-10 22:52:10 +0530

    feat: added auth

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
-import Link from "next/link";
+import { SignOut } from "@/components/auth/sign-out";
+import ScreenRecorder from "@/components/screen-recorder";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <Link href="/dashboard">Go to Dashboard</Link>
+        <ScreenRecorder />
+        <SignOut />
       </div>
     </main>
   );
