98daf700cde0c1c722ae656ccdef72840260db14
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-05-11 00:14:07 +0530

    chore: moving files

---
+++ web/src/app/(protected)/layout.tsx
@@ -0,0 +1,20 @@
+import { auth } from "@/server/auth";
+import { redirect } from "next/navigation";
+
+const ProtectedLayout = async ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+  return (
+    <div>
+      ProtectedLayout
+      {children}
+    </div>
+  );
+};
+
+export default ProtectedLayout;
