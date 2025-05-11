5ec0335724759e66c1e2ba8fea09a4c6e57aad23
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-05-11 15:24:55 +0530

    feat: data table

---
+++ web/src/components/view/video/list/columns.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { createShortUrl } from "@/server/action/shorturl"
+import type { shortUrls, videos as videoTable } from "@/server/db/schema"
+import type { ColumnDef } from "@tanstack/react-table"
+import { Clipboard, MoreHorizontal, ShareIcon } from "lucide-react"
+
+export type VideoType = {
+  video: typeof videoTable.$inferSelect
+  short_url: typeof shortUrls.$inferSelect | null
+}
+const handleShare = async (videoId: string) => {
+  const result = await createShortUrl(videoId)
+  if (result.success) {
+    console.log(result.data)
+    alert("Short url created successfully")
+  } else {
+    console.error(result.error)
+    alert("Error creating short url")
+  }
+}
+
+export const columns: ColumnDef<VideoType>[] = [
+  {
+    accessorFn: (row) => row.video.title,
+    header: "Title",
+  },
+  {
+    accessorFn: (row) => row.video.createdAt,
+    header: "Created At",
+    cell: ({ cell }) => (
+      <div>{new Date(cell.getValue() as number).toLocaleDateString()}</div>
+    ),
+  },
+  {
+    id: "actions",
+    header: "Share",
+    cell: ({ row }) => {
+      const { video, short_url } = row.original
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start">
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard.writeText(
+                  window.location.origin + `/share/${short_url?.shortVideoId}`,
+                )
+              }
+              disabled={!short_url}
+            >
+              <Clipboard className="mr-2 h-4 w-4" />
+              Copy Share Link
+            </DropdownMenuItem>
+
+            <DropdownMenuItem
+              onClick={() => handleShare(video.id)}
+              disabled={!!short_url}
+            >
+              <ShareIcon className="mr-2 h-4 w-4" />
+              Create Share Link
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
+]
---
+++ web/src/components/view/video/list/index.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { createShortUrl } from "@/server/action/shorturl"
+import { getVideos } from "@/server/action/video"
+import { useEffect, useState } from "react"
+import { columns, type VideoType } from "./columns"
+import { DataTable } from "./data-table"
+
+// This needs to be accessible by the columns
+export const handleShare = async (videoId: string) => {
+  const result = await createShortUrl(videoId)
+  if (result.success) {
+    console.log(result.data)
+    alert("Short url created successfully")
+  } else {
+    console.error(result.error)
+    alert("Error creating short url")
+  }
+}
+
+const VideoList = () => {
+  const [videos, setVideos] = useState<VideoType[]>([])
+
+  useEffect(() => {
+    async function getData() {
+      const result = await getVideos()
+      if (result.success && result.data) {
+        setVideos(result.data)
+      } else {
+        console.error(result.error)
+      }
+    }
+    getData().catch(console.error)
+  }, [])
+
+  if (videos.length === 0) {
+    return <p className="text-center text-gray-500">Loading</p>
+  }
+
+  return (
+    <div className="mx-auto w-full p-6">
+      <DataTable columns={columns} data={videos} />
+    </div>
+  )
+}
+
+export default VideoList
